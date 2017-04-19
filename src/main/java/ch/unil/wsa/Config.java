5e90fee2107511f9f78b7bbf6a4976e203d32542
Qathom <tony9295@gmail.com>
2017-04-19 15:02:51 +0200

    Add default Config class

---
+++ src/main/java/ch/unil/wsa/Config.java
@@ -0,0 +1,9 @@
+package ch.unil.wsa;
+
+public class Config {
+
+	public static String CONSUMER = "";
+	public static String CONSUMER_SECRET= "";
+	public static String TOKEN ="";
+	public static String TOKEN_SECRET = "";
+}
