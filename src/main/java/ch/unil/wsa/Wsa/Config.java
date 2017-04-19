c3214b931bf04d99e610e6d82f8ba781e50540ef
Qathom <tony9295@gmail.com>
2017-04-19 15:00:03 +0200

    Remove old file

--- src/main/java/ch/unil/wsa/Wsa/Config.java
+++
@@ -1,9 +0,0 @@
-package ch.unil.wsa.Wsa;
-
-public class Config {
-
-	public static String CONSUMER = "";
-	public static String CONSUMER_SECRET= "";
-	public static String TOKEN ="";
-	public static String TOKEN_SECRET = "";
-}
